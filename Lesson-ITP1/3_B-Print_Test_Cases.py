57e131083ee3e7b0397294ddfd085a05e1cb0901
muraki111 <eitarou.0201@gmail.com>
2020-12-30 15:11:53 +0900

    0を入力した時出力しない

--- Lesson-ITP1/3_B-Print_Test_Cases.py
+++ Lesson-ITP1/3_B-Print_Test_Cases.py
@@ -2,10 +2,10 @@ mylist = []
 num = 1
 for i in range(0, 10000):
     val = input()
-    if val:
-        mylist.append(val)
     if val == "0":
         break
+    if val:
+        mylist.append(val)
 for item in mylist:
     print("Case %d: %s" % (num, item))
     num+=1
